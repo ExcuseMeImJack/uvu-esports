669fb2bb8fc7947acb70e2302603d49af855f7ee
ExcuseMeImJack <ExcuseMeImJack@gmail.com>
2025-01-06 12:28:21 -0800

    Removed fonts

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,5 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+// import { Inter } from 'next/font/google'
 
 export const metadata = {
   metadataBase: new URL('https://postgres-prisma.vercel.app'),
@@ -8,18 +8,18 @@ export const metadata = {
     'The official site for Utah Valley Esports!',
 }
 
-const inter = Inter({
-  variable: '--font-inter',
-  subsets: ['latin'],
-  display: 'swap',
-})
+// const inter = Inter({
+//   variable: '--font-inter',
+//   subsets: ['latin'],
+//   display: 'swap',
+// })
 
 export default function RootLayout({
   children,
 }) {
   return (
     <html lang="en">
-      <body className={inter.variable}>
+      <body className={"inter.variable"}>
 
         {children}
 
