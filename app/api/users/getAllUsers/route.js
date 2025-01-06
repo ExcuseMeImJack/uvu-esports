249f7aa7ce56db9575d3dcdf442d6b7e29a8db05
ExcuseMeImJack <ExcuseMeImJack@gmail.com>
2025-01-06 12:30:37 -0800

    Removed getAllUsers route for deployment

--- app/api/users/getAllUsers/route.js
+++ app/api/users/getAllUsers/route.js
@@ -1,23 +1,23 @@
-export const dynamic = 'force-static'
+// export const dynamic = 'force-static'
 
-import { neon } from '@neondatabase/serverless';
+// import { neon } from '@neondatabase/serverless';
 
-export async function GET(req) {
-  try {
-    const sql = neon(`${process.env.DATABASE_URL}`);
+// export async function GET(req) {
+//   try {
+//     const sql = neon(`${process.env.DATABASE_URL}`);
 
-  } catch (error) {
-    console.error("Error fetching user data:", error);
-    return NextResponse.json({ error: "Internal server error" }, { status: 500 });
-  }
+//   } catch (error) {
+//     console.error("Error fetching user data:", error);
+//     return NextResponse.json({ error: "Internal server error" }, { status: 500 });
+//   }
 
-  const res = await fetch('https://data.mongodb-api.com/...', {
-    headers: {
-      'Content-Type': 'application/json',
-      'API-Key': process.env.DATA_API_KEY,
-    },
-  })
-  const data = await res.json()
+//   const res = await fetch('https://data.mongodb-api.com/...', {
+//     headers: {
+//       'Content-Type': 'application/json',
+//       'API-Key': process.env.DATA_API_KEY,
+//     },
+//   })
+//   const data = await res.json()
 
-  return Response.json({ data })
-}
+//   return Response.json({ data })
+// }
