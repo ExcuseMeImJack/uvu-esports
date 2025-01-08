53d74acc524e3c447a62b78c9e99ebfffd66f633
ExcuseMeImJack <ExcuseMeImJack@gmail.com>
2025-01-08 12:39:24 -0800

    Added user auth

---
+++ app/api/users/route.js
@@ -0,0 +1,40 @@
+import prisma from '../../../lib/prisma.ts';
+import { NextResponse } from 'next/server';
+import isAdmin from '../../../lib/routes/isAdmin.js'; // Ensure isAdmin is correctly implemented
+
+// Handler for GET requests
+export async function GET(req) {
+  try {
+    // Call isAdmin and check if the user is authorized
+    const authorized = await isAdmin(req); // Pass the request to isAdmin
+    if (!authorized) {
+      return NextResponse.json(
+        { error: 'Unauthorized access' },
+        { status: 403 }
+      );
+    }
+
+    // Fetch users if the user is an admin
+    const users = await prisma.user.findMany({
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        profilePic: true,
+        isAdmin: true,
+      },
+    });
+
+    // Return a JSON response with the users list
+    return NextResponse.json(users);
+  } catch (error) {
+    console.error(error);
+    // Return a JSON response with an error message
+    return NextResponse.json(
+      { error: 'Error fetching users from the database' },
+      { status: 500 }
+    );
+  } finally {
+    await prisma.$disconnect();
+  }
+}
